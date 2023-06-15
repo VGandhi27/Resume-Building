eb93452d7421ee4e1abf10905661c1b228cb2737
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-06-15 17:24:32 +0530

    creating component file commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes,Navigate} from "react-router-dom";
+
+import Home from "./routes/Home"
+import About from "./routes/About"
+// import Blog from "./routes/Blog"
+import Contact from './routes/Contact'
+import Project from './routes/Project'
+// import Skills from './routes/Skills'
+// import Timelines from './routes/Timelines'
 
 const App = () => {
   return (
-    <div>Home</div>
+    <>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          {/* <Route index element={<><Home  /></>} /> */}
+            <Route exact path="contact" element={<Contact  />} />
+            {/* <Route  exact path="blog" element={<Blog />} /> */}
+            <Route exact path="project" element={<Project />} />
+            <Route path="about" element={<About />} />
+            {/* <Route path="timelines" element={<Timelines />} />
+            <Route path="skills" element={<Skills />} /> */}
+            <Route path="*" element={<Navigate to="/"/>} />
+           
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
