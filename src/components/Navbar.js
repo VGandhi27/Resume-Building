d34858c1bb74a26fae8511474187441e41da77fd
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-07-29 21:47:18 +0530

    homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,7 +52,7 @@ const Navbar = () => {
           </li>
           
         </ul>
-      
+       
         <div className="hamburger" onClick={handleClick}>
           {click ?(<FaTimes size={20} style={{color:"#fff"}} />
           ):(<FaBars size={20} style={{color:"#fff"}} />)
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroImg2 from '../components/HeroImg2'
-import HeroImg3 from '../components/HeroImg3'
-import HeroImg4 from '../components/HeroImg4'
+// import HeroImg3 from '../components/HeroImg3'
+// import HeroImg4 from '../components/HeroImg4'
 import Features from '../components/Features'
-import Testimonial from '../components/Testimonial'
+// import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
     <div>
-       {/* <Navbar/> */}
-       <HeroImg2/>
+       <Navbar/>
+        <HeroImg2/>
        <Features/>
-       <HeroImg3/>
-       <HeroImg4/>
-       <Testimonial/>
+       {/* <HeroImg3/> */}
+       {/* <HeroImg4/> */}
+       {/* <Testimonial/> */}
     </div>
   )
 }
