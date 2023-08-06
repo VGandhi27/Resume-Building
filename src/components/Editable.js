10a5971cb9eee718978f5ecd4a7008507b407e63
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-08-06 15:47:54 +0530

    resume css

--- src/components/Editable.js
+++ src/components/Editable.js
@@ -6,6 +6,15 @@ const editable = () => {
   return (
     <>
      <div className="edit-resume">
+      <section className="title" >
+        <h1 className='intro_name'>Vidushi Gandhi</h1>
+        <h3>Developer</h3>
+        <ptopic>
+          <phone>+918338838366</phone>
+          <email>vgddhvd@sbjh.com</email>
+        </ptopic>
+
+      </section>
         </div> 
     </>
   )
